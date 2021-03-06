bbbe63748f7e79df73b1088b6fc66ab86de2e3df
bayuwijaya <bayuwijaya71@yahoo.co.id>
2021-03-06 19:11:29 +0800

    revisi_002

--- app/views/fileform.blade.php
+++ app/views/fileform.blade.php
@@ -2,34 +2,34 @@
 @extends('layout')
 
 @section('content')
-        <div class="container">
-        <div class="col-md-5 col-md-offset-3">
-         @if (Session::has('pesan'))
-        {{ Session::get('pesan') }}
+<div class="container">
+<div class="col-md-5 col-md-offset-3">
+ @if(Session::has('pesan'))
+{{ Session::get('pesan') }}
 
-        @endif
-        <h2>File Upload</h2>
-        <hr />
-         
-         @if (Session::has('success'))
-        <div class="alert-box success">
-        <h2>{{ Session::get('success') }}</h2>
-        </div>
-        @endif
+@endif
+<h2>File Upload</h2>
+<hr />
+ 
+ @if(Session::has('success'))
+<div class="alert-box success">
+<h2>{{ Session::get('success') }}</h2>
+</div>
+@endif
 
-        <p class="errors">{{ $errors->first('image') }}</p>
-         @if (Session::has('error'))
-         <p class="errors">{{ Session::get('error') }}</p>
-         @endif
+<p class="errors">{{$errors->first('image')}}</p>
+	@if(Session::has('error'))
+	<p class="errors">{{ Session::get('error') }}</p>
+	@endif
 
-        {{ Form::open(['action' => 'ValidateController@upload', 'files' => true]) }}
-        {{ Form::label('upload', 'Upload File') }}
-        {{ Form::file('image', '', ['class' => 'form-control']) }}
-        <br>
-        {{ Form::submit('Login', ['class' => 'btn btn-primary']) }}
-        {{ Form::close() }}
-        </div>
-        </div>
+{{Form::open(array('action' => 'ValidateController@upload', 'files'=>TRUE)) }}
+{{Form::label('upload', 'Upload File') }}
+{{Form::file('image', '', array('class' => 'form-control'))}}
+<br>
+{{Form::submit('Login', array('class' => 'btn btn-primary')) }}
+{{Form::close() }}
+</div>
+</div>
 @stop
 ?>
 </body>
