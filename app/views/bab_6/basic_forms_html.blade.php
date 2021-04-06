d9017297f3120da6983ac71b39c58ef8d590f91e
bayuwijaya <bayuwijaya71@yahoo.co.id>
2021-04-06 16:59:18 +0800

    Komponen Form

--- app/views/bab_6/basic_forms_html.blade.php
+++ app/views/bab_6/basic_forms_html.blade.php
@@ -56,6 +56,49 @@
         {{ Form::close() }}
         <br />
         <!-- Form Binding ke Model -->
-        {{ Form::model($data, array('route'=>'update', $item->id)) }}
+            <!-- Form::model($data, array('route'=>'login.showLogin', $item->id)) -->
+        <!-- Komponen Form -->
+        <!-- Elemen label -->
+        <p> {{ Form::label('revenue', 'Revenue Monthly', array('class'=>'awesome')) }} </p>
+        <!-- Input Text -->
+        <p> {{ Form::text('revenue', 'Rp.2.500.000.000', array('name'=>'pendapatan', 'class'=>'form-control')) }} </p>
+        <!-- Input TextArea -->
+        <p> {{ Form::textarea('alamat', 'Alamat Rumah', array('class'=>'form-control')) }} </p>
+        <!-- Input Password --> 
+        <p> {{ Form::password('kata_sandi', array('placeholder'=>'Password / Kata Sandi', 'class'=>'form-control')) }} </p>
+        <!-- Input Hidden -->
+        {{ Form::hidden('piutang', 'Rp.500.000.000', array('id'=>'accounts_receivable')) }}
+        <!-- Check Box -->
+        <p> 
+            {{ Form::checkbox('isRead', '1', false, array('id'=>'pernah_dibaca', 'class'=>'form-control')) }} First  
+            <br />
+            {{ Form::checkbox('isRead', '2', false, array('id'=>'pernah_dibaca', 'class'=>'form-control')) }} Second
+            <br />
+            {{ Form::checkbox('isRead', '3', true, array('id'=>'pernah_dibaca', 'class'=>'form-control')) }} Third
+        </p>
+        <!-- Radio Button -->
+        <p>
+            {{ Form::radio('color', 'red', false, array('id'=>'warna', 'class'=>'form-control')) }} Red
+            <br />
+            {{ Form::radio('color', 'black', true, array('id'=>'warna', 'class'=>'form-control')) }} Black
+            <br />
+            {{ Form::radio('color', 'blue', false, array('id'=>'warna', 'class'=>'form-control')) }} Blue
+        </p>
+        <!-- Select Box / Drop Down Lists -->
+        <p>
+            {{ Form::select('color', array('red'=>'Merah', 'black'=>'Hitam', 'blue'=>'Biru'), 'black', 
+                            array('id'=>'dropdown_color', 'class'=>'form-control')) }}
+            <br /> <br />
+            {{ Form::select('size', array('L'=>'Large', 'S'=>'Small'), 'S', array('id'=>'dropdown_size', 'class'=>'form-control')) }}
+            <br /> <br />
+            {{ Form::select('animal', array('Cats' => array('lpd'=>'Leopard', 'jgr'=>'Jaguar', 'cth'=>'Cheetah'),
+                                            'Dogs' => array('spl'=>'Spaniel', 'sbh'=>'Siberian Husky', 'ptb'=>'Pit Bull', 'blg'=>'Buldog'),
+                                            'Whale'=> array('bw'=>'Blue Whale', 'kw'=>'Killer Whale', 'sw'=>'Sperm Whale')), 
+                            'kw', array('id'=>'dropdown_animal', 'class'=>'form-control')) }}
+        </p>
+        <!-- Email Input -->
+        <p>
+            {{ Form::email('email', 'greet@email.com') }}
+        </p>
     </body>
 </html>
